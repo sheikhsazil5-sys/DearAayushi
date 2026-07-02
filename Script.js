723700dff956d55d1c7e8c5123b65e95403141e9
Sheikh sazil <sheikhsazil5@gmail.com>
2026-07-03 00:32:01 +0530

    Implement cursor glow on mouse move
    Add cursor glow effect that follows mouse movement.

--- Script.js
+++ Script.js
@@ -42,3 +42,9 @@ function createHeart() {
 }
 
 setInterval(createHeart, 700);
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
