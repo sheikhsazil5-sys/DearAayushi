ef551bb368add13bf1a5728730ba78b600fe466d
Sheikh sazil <sheikhsazil5@gmail.com>
2026-07-03 00:18:51 +0530

    Implement button hover effect and heart animation
    Added smooth button hover effect and floating hearts animation.

--- Script.js
+++ Script.js
@@ -1,1 +1,35 @@
+// Smooth button effect
+const button = document.getElementById("openBtn");
 
+button.addEventListener("mouseenter", () => {
+    button.style.transform = "scale(1.08)";
+});
+
+button.addEventListener("mouseleave", () => {
+    button.style.transform = "scale(1)";
+});
+
+// Floating Hearts
+function createHeart() {
+
+    const heart = document.createElement("div");
+
+    heart.innerHTML = "❤";
+
+    heart.classList.add("heart");
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.fontSize = (15 + Math.random() * 20) + "px";
+
+    heart.style.animationDuration = (4 + Math.random() * 5) + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 9000);
+
+}
+
+setInterval(createHeart, 700);
