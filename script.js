2c66c11ca947f03ea9a3ebbcb3f5cae05d1a8c39
Sheikh sazil <sheikhsazil5@gmail.com>
2026-07-03 00:59:26 +0530

    Implement petal creation and animation
    Add functionality to create and animate petals.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const petals=document.querySelector(".petals");
 
+function createPetal(){
+
+const petal=document.createElement("div");
+
+petal.classList.add("petal");
+
+petal.style.left=Math.random()*100+"vw";
+
+petal.style.animationDuration=(6+Math.random()*5)+"s";
+
+petal.style.opacity=Math.random();
+
+petals.appendChild(petal);
+
+setTimeout(()=>{
+petal.remove();
+},11000);
+
+}
+
+setInterval(createPetal,300);
