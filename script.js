29705d673f4c875b7f0176b44b229d6854bd2879
Sheikh sazil <sheikhsazil5@gmail.com>
2026-07-04 01:30:03 +0530

    Implement image popup feature
    Added functionality to display a popup with an image when clicked and close it.

--- script.js
+++ script.js
@@ -144,3 +144,33 @@ section.style.transition="1s";
 observer.observe(section);
 
 });
+const popup = document.getElementById("imagePopup");
+const popupImg = document.getElementById("popupImg");
+const closeImage = document.getElementById("closeImage");
+
+document.querySelectorAll(".photo img").forEach(img=>{
+
+    img.addEventListener("click",()=>{
+
+        popup.style.display="flex";
+        popupImg.src=img.src;
+
+    });
+
+});
+
+closeImage.addEventListener("click",()=>{
+
+    popup.style.display="none";
+
+});
+
+popup.addEventListener("click",(e)=>{
+
+    if(e.target===popup){
+
+        popup.style.display="none";
+
+    }
+
+});
